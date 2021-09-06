fe95cdcecfabe0172a54945ac0f8bf6d1abc071e
pauloalchemist <paulolinsdev@gmail.com>
2021-09-06 14:59:21 -0300

    added method create e and private method post_params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,16 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
